de365cf91101fcf5a365f53a01d9b82365984a14
Raimu0319 <raimu-nakayama-k23h@stu.kbc.ac.jp>
2025-10-20 18:05:01 +0900

    NPC_Characterの追加
    Playerが近づいたかどうかの判定の追加

--- sazandora/Source/sazandora/GameMain/NPC_Character.cpp
+++ sazandora/Source/sazandora/GameMain/NPC_Character.cpp
@@ -2,6 +2,8 @@
 
 
 #include "NPC_Character.h"
+#include "GameFramework/PlayerController.h"
+#include "Main_Character.h"
 
 // Sets default values
 ANPC_Character::ANPC_Character()
@@ -9,6 +11,23 @@ ANPC_Character::ANPC_Character()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	TalkRange = CreateDefaultSubobject<USphereComponent>(TEXT("Spheres"));
+	TalkRange->SetupAttachment(RootComponent);
+	TalkRange->SetSphereRadius(200.0f);
+	TalkRange->SetCollisionProfileName(TEXT("Trigger"));
+
+	// Overlapイベントをバインド
+	TalkRange->OnComponentBeginOverlap.AddDynamic(this, &ANPC_Character::OnPlayerEnterRange);
+	TalkRange->OnComponentEndOverlap.AddDynamic(this, &ANPC_Character::OnPlayerLeaveRange);
+
+	// meshをセット
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> MeshObj(TEXT("/Game/Characters/Mannequins/Meshes/SKM_Manny"));
+	if (MeshObj.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(MeshObj.Object);
+		GetMesh()->SetRelativeLocation(FVector(0.0f, 0.0f, -90.0f));
+		GetMesh()->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
+	}
 }
 
 // Called when the game starts or when spawned
@@ -25,10 +44,22 @@ void ANPC_Character::Tick(float DeltaTime)
 
 }
 
-// Called to bind functionality to input
-void ANPC_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+// 会話開始関数（プレイヤーが近づいたときなどに呼ぶ）
+void ANPC_Character::OnPlayerEnterRange(UPrimitiveComponent* OverlappedComp, AActor* OtherActer,
+	UPrimitiveComponent* OtherComo, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	if (OtherActer->IsA(AMain_Character::StaticClass()))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green, TEXT("Player is in"));
+	}
 }
 
+void ANPC_Character::OnPlayerLeaveRange(UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherActor->IsA(AMain_Character::StaticClass()))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, TEXT("Player is out"));
+	}
+
+}
--- sazandora/Source/sazandora/GameMain/NPC_Character.h
+++ sazandora/Source/sazandora/GameMain/NPC_Character.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Components/SphereComponent.h"
 #include "NPC_Character.generated.h"
 
 UCLASS()
@@ -23,7 +24,18 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// プレイヤーと会話できる範囲を検出するコリジョン
+	UPROPERTY(VisibleAnywhere, Category="NPC")
+	class USphereComponent* TalkRange;
+
+	// 会話開始関数（プレイヤーが近づいたときなどに呼ぶ）
+	UFUNCTION()
+	void OnPlayerEnterRange(UPrimitiveComponent* OverlappedComp, AActor* OtherActer,
+		UPrimitiveComponent* OtherComo, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	// 離れたとき
+	UFUNCTION()
+	void OnPlayerLeaveRange(UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 };
